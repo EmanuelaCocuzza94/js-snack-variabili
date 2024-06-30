cdfcf6b083a0f3947eac732bd4bb8ccf1219eb66
EmanuelaCocuzza <emauelacocuzza94@gmail.com>
2024-06-30 17:58:16 +0200

    esercizio sulle variabili

---
+++ variabili.js
@@ -0,0 +1,21 @@
+
+// PRIMO PUNTO
+
+let number = 100
+console.log(number)
+
+// SECONDO PUNTO
+
+const pi = 3.14
+console. log (pi)
+
+// const pi =3.1416
+// console. log (pi)
+
+//Uncaught SyntaxError: Identifier 'pi' has already been declared (at variabili.js:12:7)
+//la variabile pi non è riassegnabile perchè è una costante ed è stata già dichiarata.
+
+let radius = 10
+console.log (radius)
+let circle = radius * 2 * pi
+console.log (circle)
